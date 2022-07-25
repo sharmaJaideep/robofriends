d59d39f95623498edbbcfdc6e459b25fc0b6e468
jd-007-git <108944124+jd-007-git@users.noreply.github.com>
2022-07-25 22:40:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Hello from './Hello';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+// import { robots } from './robots';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <h1>Hello world!</h1> */}
+    {/* <Hello greeting={'Hello' + 'React Ninja'}/> */}
     <App />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
